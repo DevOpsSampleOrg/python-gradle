eddd9bb4d2dbda4b329bc564ee0c780b2bb1d4b7
engel80 <prettyhip@gmail.com>
2021-09-02 23:40:34 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,15 @@
 from flask import Flask
+from flask import request
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return "<p> Hello, World 2 </p>"
+    return {
+        "host": request.host,
+        "url": request.url,
+        "message": "Hello, World"
+    }
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080)
